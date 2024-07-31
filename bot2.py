229149d14af8ef69651c264e89444862d2ebf3bf
aqwarius2003 <00victorr@gmail.com>
2024-07-31 22:59:01 +0400

    исправил очередность обработчиков

--- bot2.py
+++ bot2.py
@@ -228,7 +228,6 @@ def cancel_booking(update: Update, context: CallbackContext):
 
 # Функция обработки команды /start - выводит big_keyboard
 def start(update: Update, context: CallbackContext):
-    print('jdl;kwg')
     chat_id = update.message.chat_id
     users[chat_id] = {}
     show_big_keyboard(update, context, chat_id)
@@ -733,19 +732,17 @@ def main():
     updater = Updater(TOKEN, use_context=True, request_kwargs={'read_timeout': 10, 'connect_timeout': 5})
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_admin_choice))
-    dp.add_handler(CallbackQueryHandler(button))
-    dp.add_handler(MessageHandler(Filters.contact, handle_phone_input))
     dp.add_handler(MessageHandler(Filters.text("Записаться"), show_main_menu))
     dp.add_handler(MessageHandler(Filters.text("Мои записи"), show_my_appointments))
     dp.add_handler(MessageHandler(Filters.text("Салоны"), show_salons))
     dp.add_handler(MessageHandler(Filters.text("Мастера"), show_staffs))
     dp.add_handler(MessageHandler(Filters.text("Услуги"), show_services))
     dp.add_handler(MessageHandler(Filters.text("Администратор"), show_administration_contacts))
-    dp.add_handler(CallbackQueryHandler(cancel_booking, pattern='^cancel_booking$'))
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_admin_choice))
     dp.add_handler(CallbackQueryHandler(button))
-
-    dp.add_handler(CallbackQueryHandler(cancel_booking, pattern='^cancel_booking$'))
+    dp.add_handler(MessageHandler(Filters.contact, handle_phone_input))
+    dp.add_handler(CallbackQueryHandler(cancel_booking, pattern='^cancel_booking'))
+    dp.add_handler(CallbackQueryHandler(button))
     dp.add_handler(MessageHandler(Filters.contact, handle_contact))
     dp.add_handler(MessageHandler(Filters.regex(r'^/admin:\w+'), check_administrator))
     # dp.add_handler(CommandHandler("remind_tomorrow", remind_tomorrow_command))
